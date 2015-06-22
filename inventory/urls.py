67ca246fc7a83ed042bed7c09050fd79a2a9d204
Nengmou <nmwang125@gmail.com>
2015-06-22 00:34:20 -0400

    add inventories and shipments

--- inventory/urls.py
+++ inventory/urls.py
@@ -6,4 +6,8 @@ urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^vendors/$', views.vendor_index, name='vendor.index'),
     url(r'^vendors/(?P<v_id>\d+)/$', views.vendor_detail, name='vendor.detail'),
+    url(r'^inventories/$', views.inventory_index, name='inventory.index'),
+    url(r'^inventories/(?P<inv_id>\d+)/$', views.inventory_detail, name='inventory.detail'),
+    url(r'^shipments/$', views.shipment_index, name='shipment.index'),
+    url(r'^shipments/(?P<s_id>\d+)/$', views.shipment_detail, name='shipment.detail'),
 )
--- inventory/views.py
+++ inventory/views.py
@@ -18,3 +18,26 @@ def vendor_detail(request, v_id):
     vendor = get_object_or_404(Vendor, pk=v_id)
     return render(request, 'inventory/vendor_detail.html', {'item': vendor})
 
+
+@login_required
+def inventory_index(request):
+    items = Inventory.objects.order_by('-id')
+    return render(request, 'inventory/inventory_index.html', {'items': items})
+
+
+@login_required
+def inventory_detail(request, inv_id):
+    inventory = get_object_or_404(Inventory, pk=inv_id)
+    return render(request, 'inventory/inventory_detail.html', {'item': inventory})
+
+
+@login_required
+def shipment_index(request):
+    items = Shipment.objects.order_by('-id')
+    return render(request, 'inventory/shipment_index.html', {'items': items})
+
+
+@login_required
+def shipment_detail(request, s_id):
+    shipment = get_object_or_404(Shipment, pk=s_id)
+    return render(request, 'inventory/shipment_detail.html', {'item': shipment})
